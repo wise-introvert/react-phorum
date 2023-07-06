48b5e4739f4e757c9445882353fe65b85ca4ee80
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-07-06 11:53:36 -0400

    feat: setup apollo and chakra

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { theme } from "./utils";
+
+dayjs.extend(updateLocale);
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s ago",
+    s: "a few seconds",
+    m: "a minute",
+    mm: "%d minutes",
+    h: "an hour",
+    hh: "%d hours",
+    d: "a day",
+    dd: "%d days",
+    M: "1 mo",
+    MM: "%d mos",
+    y: "1 yr",
+    yy: "%d yrs",
+  },
+});
+dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
+dayjs.extend(localizedFormat);
+
+const client: ApolloClient<{}> = new ApolloClient<{}>({
+  uri: process.env.REACT_APP_GQL_URI || "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <h1>Hello</h1>
+      </ChakraProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
