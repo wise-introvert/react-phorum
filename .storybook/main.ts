a956a9d4abe1e0401ee87d35f64ae43be2129ebd
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-07-08 12:22:58 -0400

    fix(strybk): remove css import

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import updateLocale from "dayjs/plugin/updateLocale";
 import localizedFormat from "dayjs/plugin/localizedFormat";
-import "../src/index.css";
 
 dayjs.extend(updateLocale);
 dayjs.updateLocale("en", {
