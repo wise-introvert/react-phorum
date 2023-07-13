cefbc8053bc87bb5f629e8cfa12bf0dad18f6775
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-07-13 18:44:02 -0400

    feat: add dark mode support :tada:

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ColorModeContextType,
+  useColorMode,
+  ChakraProvider,
+  Box,
+  useColorModeValue,
+  Container,
+  ColorModeScript,
+  useTheme,
+} from "@chakra-ui/react";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import updateLocale from "dayjs/plugin/updateLocale";
 import localizedFormat from "dayjs/plugin/localizedFormat";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { theme } from "./utils";
+import { Navbar, Feed } from "./components";
 
 dayjs.extend(updateLocale);
 dayjs.updateLocale("en", {
@@ -34,29 +44,67 @@ dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 
-const client: ApolloClient<{}> = new ApolloClient<{}>({
-  uri: process.env.REACT_APP_GQL_URI || "http://localhost:4000",
-  cache: new InMemoryCache(),
-});
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <h1>Hello</h1>,
-  },
-]);
+// TODO: isloate to it's own component
+const Wrapper = () => {
+  const client: ApolloClient<{}> = new ApolloClient<{}>({
+    uri:
+      process.env.NODE_ENV == "production"
+        ? process.env.REACT_APP_PROD_GQL_URI
+        : process.env.REACT_APP_GQL_URI ?? "http://localhost:4000",
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <Box
+      w={"full"}
+      h="full"
+      p={0}
+      m={0}
+      bg={useColorModeValue(theme.colors.lightgray, theme.colors.black)}
+    >
+      <Navbar />
+      <Box h={"full"} py={4} px={"10vw"}>
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export const App = () => {
+  const client: ApolloClient<{}> = new ApolloClient<{}>({
+    uri:
+      process.env.NODE_ENV == "production"
+        ? process.env.REACT_APP_PROD_GQL_URI
+        : process.env.REACT_APP_GQL_URI ?? "http://localhost:4000",
+    cache: new InMemoryCache(),
+  });
+
+  const router = createBrowserRouter([
+    {
+      element: <Wrapper />,
+      children: [
+        {
+          path: "/",
+          element: <Feed />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript nonce={"hello"} initialColorMode={"dark"} />
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </ApolloProvider>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ChakraProvider>
-    </ApolloProvider>
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
