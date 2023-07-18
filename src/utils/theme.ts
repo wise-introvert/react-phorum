a6123ac69d158a68884cbcec98b8c6e785eb5c2b
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-07-18 15:06:13 -0400

    refactor: add shades for primary color, restructure

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,27 +1,59 @@
-import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import {
+  extendTheme,
+  ThemeConfig,
+  withDefaultColorScheme,
+} from '@chakra-ui/react';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 };
 
-const font: string = 'Fira Code';
-const brand: string = 'CaslonGraD';
+// color palette
+const primary: string = '#FFBE0B';
+const primaryLight: string = '#ffe461';
+const primaryDark: string = '#e0a90b';
+const white: string = '#ffffff'; // "#fafbfb",
+const black: string = '#010001';
+const darkgray: string = '#171717';
+const lightgray: string = '#667581';
+const lightergray: string = '#EBEBEB';
 
-export const theme = extendTheme({
-  fonts: {
-    brand,
-    heading: font,
-    body: font,
-    mono: font,
+// fonts
+const font: string = 'Fira Code';
+const brandFont: string = 'CaslonGraD';
+
+export const theme = extendTheme(
+  {
+    fonts: {
+      brand: brandFont,
+      heading: font,
+      body: font,
+      mono: font,
+    },
+    colors: {
+      brand: '#FFBE0B',
+      yellow: {
+        100: '#fff7d0',
+        200: '#ffe999',
+        300: '#ffdd61',
+        400: '#ffcf2a',
+        500: '#ffbe0b', // Base color
+        600: '#e0a90b',
+        700: '#c0970a',
+        800: '#a08309',
+        900: '#825f08',
+      },
+      primary,
+      primaryLight,
+      primaryDark,
+      white, // "#fafbfb",
+      black,
+      darkgray,
+      lightgray,
+      lightergray,
+    },
+    config,
   },
-  colors: {
-    primary: '#FFBE0B',
-    white: '#ffffff', // "#fafbfb",
-    black: '#010001',
-    darkgray: '#171717',
-    lightgray: '#667581',
-    lightergray: '#EBEBEB',
-  },
-  config,
-});
+  withDefaultColorScheme({ colorScheme: 'yellow' })
+);
